4cbd2089db9ebf1022a2fc89426c23187628d246
Jon Tornetta <jmtornetta@gmail.com>
2021-08-04 18:38:14 -0400

    add header to template to call variables so that log can be written to from function output

--- template.sh
+++ template.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-main () {
-    set -Eeuo pipefail
-    trap cleanup SIGINT SIGTERM ERR EXIT
+defines () {
     local DIR
     DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
     export LOG="$DIR/${BASH_SOURCE[0]}.log"
+main () {
+    set -Eeuo pipefail
+    trap cleanup SIGINT SIGTERM ERR EXIT
 usage() {
     cat <<- EOF
     USAGE: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
@@ -44,7 +45,6 @@ EOF
         trap - SIGINT SIGTERM ERR EXIT
         # script cleanup here
     }
-
     setupColors() {
         if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
             NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
@@ -53,7 +53,6 @@ EOF
             NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
         fi
     }
-
     msg() {
         echo >&2 -e "${1-}"
     }
@@ -98,13 +97,15 @@ EOF
     parseParams "$@"
     setupColors
 
-    # script logic here
+    # INSERT script logic here
 
     msg "${RED}Read parameters:${NOFORMAT}"
     msg "- flag: ${flag}"
     msg "- param: ${param}"
     msg "- arguments: ${args[*]-}"
 }
-
+}
+# call function, set variables, and write to log file
+defines
 printf '\n\n%s' "$(date)" >> "$LOG"
-main "$@" |& tee -a "$LOG" # Execute and add StdOut and StdErr to log File
+main "$@" |& tee -a "$LOG"

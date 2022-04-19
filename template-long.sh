5eb2fd42001d1f709d508c1f508ebc0ecd89857b
Jon Tornetta <jmtornetta@gmail.com>
2022-04-19 14:41:43 -0400

    Clean 'template-long.sh' Add parameter format checking Remove 'color' option Simplify usage notes Add sample required parameter pattern '-p1' to the top Move 'msg' above trap; Replace 'printf' with 'msg'.

--- template-long.sh
+++ template-long.sh
@@ -3,12 +3,14 @@
 # Usage: Type -h or --help for usage instructions
 
 start() { # collapse this function for readability
+    # Configuration
     declare -ir reqArgs=1 # enter number of required arguments for module
-    declare -ar reqParams=() # enter number of required parameters for module
+    declare -ar reqParams=( "-p1|--param1" ) # enter regex pattern for required parameters for module
+    # Declarations
     declare -a args # container for arguments less parameters
     declare -A params # associative array container for key-values of parameters
     declare -i flag
-
+    # Setup
     declare -r DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
     declare -r SCRIPT=$(basename "${BASH_SOURCE[0]}") # script name
     declare -r nSCRIPT=${SCRIPT%.*} # script name without extension (for log)
@@ -19,18 +21,6 @@ start() { # collapse this function for readability
     body() {
         set -Eeuo pipefail
         trap cleanup SIGINT SIGTERM ERR EXIT
-        cleanup() {
-            # add additional script cleanup commands here
-            printf "\n%s\n" "Exit Note: Script cleanup complete."
-        }
-        setupColors() {
-            if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-                NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-            else
-                # shellcheck disable=SC2034  # Unused variables left for readability
-                NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-            fi
-        }
         msg() {
             # puts 'printf' delim second, assigns default, and redirects to stderr so only shows in console/log (not script output)
             # shellcheck disable=SC2059
@@ -40,9 +30,13 @@ start() { # collapse this function for readability
                 printf >&2 "%s\n" "${@}"
             fi
         }
+        cleanup() {
+            # add additional script cleanup commands here
+            printf "\n%s\n" "Exit Note: Script cleanup complete."
+        }
         die() {
-            declare -r err=$1
-            declare -ir code=${2-1} # default exit status 1
+            declare -r err="$1"
+            declare -ir code="${2-1}" # default exit status 1
             msg "$err"
             exit "$code"
         }
@@ -54,17 +48,19 @@ start() { # collapse this function for readability
                 case "${1-}" in # '${1-}' sets default to 'null'
                 -h | --help) usage ;;
                 -v | --verbose) set -x ;;
-                -f | --flag) flag=1 && printf "\n%s\n" "Flag set successful." ;; # example flag
+                -f | --flag) flag=1 && msg "Flag set successful." ;; # a flag is a binary parameter (i.e. it has no value)
                 -p1 | --param1) # example; copy-paste this case for more parameters
-                    params["$1"]="${2-}"
+                    ! [[ "${2:-}" =~ ^[a-zA-Z0-9[:blank:]]{1,100}$ ]] && die "Parameter value invalid." # Check paramter format using regex here
+                    params["$1"]="$2"
+                    msg "P1 parameter set!" # Sample action
                     shift # removes the 'param' value from the array of arguments supplied to script so additional arguments can be processed
                     ;;
                 -p2 | --param2) # example; copy-paste this case for more parameters
-                    params["$1"]="${2-}"
+                    ! [[ "${2:-}" =~ ^[a-zA-Z0-9[:blank:]]{1,100}$ ]] && die "Parameter value invalid." # Check paramter format using regex here
+                    params["$1"]="$2"
+                    msg "P2 parameter set!" # Sample action
                     shift # removes the 'param' value from the array of arguments supplied to script so additional arguments can be processed
                     ;;
-
-                --no-color) NO_COLOR=1 ;;
                 -?*) die "Unknown option: $1" ;;
                 *) break ;;
                 esac
@@ -90,27 +86,27 @@ start() { # collapse this function for readability
         }
         import() {
             # shellcheck source=/dev/null
-            { [[ -f "$1" ]] && source "$@" ; } || msg "Import file does not exist"
+            { [[ -f "$1" ]] && source "$@" ; } || die "Import file does not exist"
         }
         usage() {
             cat <<-EOF
-USAGE: $SCRIPT [-h] [-v] [-f] -p param_value arg1 [arg2...]
+USAGE: $SCRIPT -p1 param_value [-p2 param_value] [-h] [-v] [-f] arg1 [arg2...]
 
 Program description goes here.
 
+ARGUMENTS:
+1) Required. Argument description.
+2) Optional. Argument description.
+
 OPTIONS:
--c --config              configuration file containing the rules. use --help-config to see the syntax.
--n --pretend             do not really delete, just how what you are going to do.
--t --test                run unit test to check the program
--v --verbose             Verbose. You can specify more then one -v to have more verbose
--x --debug               debug
--h --help                show this help
-    --help-config         configuration help
+-v --verbose             Verbose shows line-by-line module messages.
+-h --help                Help shows this usage message.
+-f --flag                Flag sets a binary switch for the module.
+-p1 --param1             Some required parameter.
+-p2 --param2             Some optional parameter.
 
 EXAMPLES:
 1) Example 1
-2) Example 2
-3) Example 3
 
 EOF
             trap '' EXIT # unsets the exit trap when '--help' is defined
@@ -118,17 +114,18 @@ EOF
         }
 
         parseParams "$@" # called after 'usage' is defined
-        setupColors
 
         #~~~ BEGIN SCRIPT ~~~#
-        import "$DIR/vars" # example; include source vars/files here
-        printf "\n%s\n" "Hello World!"
+
+        # import "$DIR/vars" # example; include source vars/files here
+        msg "\n%s\n\n" "Hello World!"
+
         #~~~ END SCRIPT ~~~#
 
-        msg "${RED}Inputs for '$SCRIPT' in '$DIR'...${NOFORMAT}" "\n%s\n"
+        msg "Inputs for '$SCRIPT' in '$DIR'..."
         msg "- arguments: $(joinArr ", " "${args[@]}")" # joins arguments array into delimited string
-        msg "- flag: ${flag}"
         msg "- parameters: $(declare -a arr; for key in "${!params[@]}"; do arr+=("$key:${params[$key]}"); done; joinArr ", " "${arr[@]}")" # joins parameters array into delimited string of key-value pairs
+        msg "- flag: ${flag}"
     }
     printf '\n\n%s\n\n' "---$(date)---" >>"$LOG"
     body "$@" |& tee -a "$LOG" # pass arguments to functions and stream console to log

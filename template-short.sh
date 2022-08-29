0206493c546c51eded029554b4d55b040ffc8d8b
Jon Tornetta <jmtornetta@gmail.com>
2022-08-29 15:15:37 -0400

    Refactor: Simplify short template

--- template-short.sh
+++ template-short.sh
@@ -6,16 +6,18 @@ start() {
     declare -r DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
     declare -r SCRIPT=$(basename "${BASH_SOURCE[0]}") # script name
     declare -r nSCRIPT=${SCRIPT%.*} # script name without extension (for log)
-    declare -r TODAY=$(date +"%Y%m%d" | sed 's/^[2-9][0-9]//') # removes first two digits from year
-    declare -r LOG="/tmp/$TODAY-$nSCRIPT.log"
-    cd "$DIR" # ensure in this function's directory
+    # declare -r TODAY=$(date +"%Y%m%d" | sed 's/^[2-9][0-9]//') # removes first two digits from year
+    # declare -r LOG="/tmp/$TODAY-$nSCRIPT.log"  # Optionally include a log file
+    # cd "$DIR" # ensure in this function's directory
 
     body() {
+        echo 1>&2 "Running '$nSCRIPT' in '$DIR' from '$(pwd)'..." # Redirect to STDERR so it isn't included in script output
         #~~~ BEGIN SCRIPT ~~~#
         printf "\n%s\n" "Hello World!"
         #~~~ END SCRIPT ~~~#
     }
-    printf '\n\n%s\n\n' "---$(date)---" >>"$LOG"
-    body "$@" |& tee -a "$LOG" # pass arguments to functions and stream console to log; NOTE: do not use 'tee' with 'select' menus!
+    # printf '\n\n%s\n\n' "---$(date)---" >>"$LOG"  # Optionally copy STDOUT and STDERR to log
+    # body "$@" |& tee -a "$LOG" # Optionally copy stream to log; NOTE: do not use 'tee' with 'select' menus!
+    body "$@"
 }
 start "$@"
